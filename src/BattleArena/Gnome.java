57babb40c0361ed19d5449ea466a7d25464b18ab
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-02-19 10:59:50 +0100

    attributes and Getter and Setter of Gnome

--- src/BattleArena/Gnome.java
+++ src/BattleArena/Gnome.java
@@ -1,5 +1,32 @@
 package BattleArena;
 
 public class Gnome {
+	
+	private String Name;
+	private int Lifepoints;
+	private boolean SpecialAbilityActive;
+	
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getLifepoints() {
+		return Lifepoints;
+	}
+	public void setLifepoints(int lifepoints) {
+		Lifepoints = lifepoints;
+	}
+	public boolean isSpecialAbilityActive() {
+		return SpecialAbilityActive;
+	}
+	public void setSpecialAbilityActive(boolean specialAbilityActive) {
+		SpecialAbilityActive = specialAbilityActive;
+	}
+	
+	
+	
 
 }
