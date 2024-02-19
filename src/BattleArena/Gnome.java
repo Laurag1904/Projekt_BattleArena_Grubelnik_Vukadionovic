2188dcb2dfa9a5bf4e002c778f08ff71e2712e7a
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-02-19 11:31:39 +0100

    Methode created get Damage

--- src/BattleArena/Gnome.java
+++ src/BattleArena/Gnome.java
@@ -1,5 +1,8 @@
 package BattleArena;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Gnome {
 	
 	private String Name;
@@ -27,6 +30,20 @@ public class Gnome {
 	}
 	
 	
+	public void getDamage(int points) {
+		if (points > 0) {
+            this.Lifepoints -= points;
+            if (this.Lifepoints < 0) {
+                this.Lifepoints = 0; // Lebenspunkte können nicht negativ sein
+            }
+            System.out.println(this.Name + " hat " + points + " Schaden erlitten!");
+        }
+	}
 	
-
+	public void attack () {
+		ThreadLocalRandom.current().nextInt(min, max + 1);
+		Random rand = new Random ();
+		
+	}
+	
 }
