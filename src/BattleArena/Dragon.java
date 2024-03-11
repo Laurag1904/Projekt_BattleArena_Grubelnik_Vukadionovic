5cf35b72963f35d9b334611fb653517d9dc4c96d
marinavukadinovic <marina.vukadinovic@hak-reutte.ac.at>
2024-03-11 10:15:15 +0100

    Dragon class + java doc.comments (complete)

--- src/BattleArena/Dragon.java
+++ src/BattleArena/Dragon.java
@@ -3,43 +3,51 @@ package BattleArena;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Dragon { 
-	
-	private String name;
-	private int lifePoints;
-	private boolean specialAbilityActive;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getLifePoints() {
-		return lifePoints;
-	}
-	public void setLifePoints(int lifePoints) {
-		this.lifePoints = lifePoints;
-	}
-	public boolean isSpecialAbilityActive() {
-		return specialAbilityActive;
-	}
-	public void setSpecialAbilityActive(boolean specialAbilityActive) {
-		this.specialAbilityActive = specialAbilityActive;
-	}
-	
-	public void getDamage(int points) {
-        if (points > 0) {
-            this.lifePoints -= points;
-            if (this.lifePoints < 0) {
-                this.lifePoints = 0; // Lebenspunkte können nicht negativ sein
-            }
-            System.out.println(this.name + " hat " + points + " Schaden erlitten!");
-        }
-    }
-	
+public class Dragon extends FightCharecter{
 
-	public void attack(Character enemy) {
-		ThreadLocalRandom.current().nextInt(min, max + 1);
-        Random rand = new Random ();
 
-	}}
+	public Dragon(String name) {
+		super(name);	
+			
+	}
+	
+	/**
+	 * Attacknumber between 20 and 25 whick is randomly added at every attack, is being subracted from the opponed when attacked
+	 */
+	/*
+	 * Malus at basevalue if specialAbility is active, random number between 5 and 10
+	 */
+	public void attack() {
+		int attackNumber = ThreadLocalRandom.current().nextInt(20,25);
+		if (this.isSpecialAbilityActive()== true) {
+			attackNumber = attackNumber- ThreadLocalRandom.current().nextInt(5,10);
+		}
+	}
+
+
+	/**
+	 * If specialAbility is active +10 points
+	 */
+	public boolean specialAbilityActive() {
+		this.setExtraLife(getExtraLife()+10);
+		this.setSpecialAbilityActive(true);
+		return true;
+	
+	}
+
+
+	
+	/**
+	 * If specialAbility is deactivated subtract Bonus and Malus if points are greater than 0 else set Points as 0
+	 */
+	public boolean specialAbilityDeactivated() {
+		this.setSpecialAbilityActive(false);
+		if (this.getExtraLife() - 10 >= 0) {
+			this.setExtraLife(getExtraLife()-10);
+		}else {
+			this.setExtraLife(0);
+		}
+		return true;	
+	}
+}
+
