a0f37eee6910a1a03a86716f1aab4fe8cc8b20f2
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-03-15 12:43:18 +0100

    Created attributes for the class app

--- src/BattleArena/App.java
+++ src/BattleArena/App.java
@@ -1,5 +1,17 @@
 package BattleArena;
 
+import java.util.Scanner;
+
 public class App {
+	
+	public static void main(String [] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int choice  = 0;
+		String name = "";
+		
+		FightCharecter p1 = null;
+		FightCharecter p2 = null;
+	}
 
 }
