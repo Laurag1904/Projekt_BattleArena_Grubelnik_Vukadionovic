3aa3ca01ddc72cdb12ee3dfd12d77aa52a41b0c1
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-03-17 12:13:56 +0100

    ethode for Special Skills

--- src/BattleArena/Gnome.java
+++ src/BattleArena/Gnome.java
@@ -9,12 +9,36 @@ public abstract class Gnome extends FightCharecter {
 		super(name);
 		
 	}
+	public String getName() {
+		return "Gnome";
+	}
+	
+	
 	public void getDamage(int points) {
 	}
 	
 	public void attack () {
+		int attackNumber = ThreadLocalRandom.current().nextInt(20,25);
+		if(this.isSpecialAbilityActive()) {
+			double random = Math.random();
+			
+		}
 	} 
 	
+	/**
+	 * Tis ability activates only if, the Gnome current lifepoints are less then 50 or equal.
+	 * @return true if it is activated, false if it is not
+	 */
+	
+	public boolean activateSpecialSkills() {
+		if(this.getLifepoints()<= 50) {
+			this.setSpecialAbilityActive(true);
+			return true;
+		}else {
+			this.setSpecialAbilityActive(false);
+			return false;
+		}
+	}
 	
 	
 	 
